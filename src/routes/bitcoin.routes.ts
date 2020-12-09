ede8307bac793b47ecd3261a7b82297ecb2b9b57
Daniel Oliveira <oliveiradaniel@protonmail.com>
2020-12-09 07:38:36 -0300

    feat: add currency update

--- src/routes/bitcoin.routes.ts
+++ src/routes/bitcoin.routes.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import AxiosNetwork from '../network/AxiosNetwork';
 import CoindeskBitcoinPriceIndex from '../providers/implementations/CoindeskBitcoinPriceIndex';
 import GetCurrenciesService from '../services/GetCurrenciesService';
+import UpdateCurrenciesService from '../services/UpdateCurrenciesService';
 
 const bitcoinRouter = Router();
 const getCurrenciesService = new GetCurrenciesService();
+const updateCurrenciesService = new UpdateCurrenciesService();
 const network = new AxiosNetwork('https://api.coindesk.com/v1/bpi');
 const bitcoinPriceIndex = new CoindeskBitcoinPriceIndex(
   getCurrenciesService,
@@ -15,6 +17,21 @@ bitcoinRouter.get('/', async (request, response) => {
   const priceIndex = await bitcoinPriceIndex.getCurrentPrice();
 
   return response.json(priceIndex);
+});
+
+bitcoinRouter.post('/', async (request, response) => {
+  try {
+    const { currency, value } = request.body;
+
+    await updateCurrenciesService.execute({
+      currency,
+      value,
+    });
+
+    return response.json({ message: 'Valor alterado com sucesso!' });
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
 });
 
 export default bitcoinRouter;
---
+++ src/services/UpdateCurrenciesService.ts
@@ -0,0 +1,61 @@
+import fs from 'fs';
+import { resolve } from 'path';
+import * as yup from 'yup';
+
+interface CurrenciesDTO {
+  [key: string]: string;
+}
+
+interface UpdateCurrenciesDTO {
+  currency: string;
+  value: string;
+}
+
+class UpdateCurrenciesService {
+  private currenciesPath: string;
+
+  private allowedCurrencies: string[];
+
+  constructor() {
+    this.currenciesPath = resolve(
+      __dirname,
+      '..',
+      'helpers',
+      'currencies.json',
+    );
+    this.allowedCurrencies = ['BRL', 'EUR', 'CAD'];
+  }
+
+  public async execute({
+    currency,
+    value,
+  }: UpdateCurrenciesDTO): Promise<void> {
+    try {
+      const schema = yup.object().shape({
+        currency: yup.string().required().oneOf(this.allowedCurrencies),
+        value: yup.number().required().integer().positive(),
+      });
+
+      await schema.validate({
+        currency,
+        value,
+      });
+    } catch (err) {
+      if (err.path === 'value') {
+        throw new Error('Valor inválido');
+      } else {
+        throw new Error('Moeda inválida');
+      }
+    }
+    const currencies = fs.readFileSync(this.currenciesPath, {
+      encoding: 'utf-8',
+    });
+    const currenciesParsed: CurrenciesDTO = JSON.parse(currencies);
+
+    currenciesParsed[currency] = value;
+
+    fs.writeFileSync(this.currenciesPath, JSON.stringify(currenciesParsed));
+  }
+}
+
+export default UpdateCurrenciesService;
