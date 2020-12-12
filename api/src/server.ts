0c1c7b19168ea8b16b6419d3be089ea3b6eaf6f0
Daniel Oliveira <oliveiradaniel@protonmail.com>
2020-12-11 23:24:34 -0300

    feat(api): add cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
 const PORT = 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 app.use((request, response) => {
